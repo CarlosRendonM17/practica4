4a9c026e55b53bf41cd4e781e611d0bffc2c7545
carlos mario rendon martinez <carlos.rendonm1@udea.edu.co>
2025-10-27 22:30:09 -0500

    Estructura base del proyecto y clases vacías

--- Red.h
+++ Red.h
@@ -1,4 +1,16 @@
 #ifndef RED_H
 #define RED_H
 
-#endif // RED_H
+#include "Enrutador.h"
+#include <unordered_map>
+using namespace std;
+
+class Red {
+private:
+    unordered_map<string, Enrutador> enrutadores;
+
+public:
+    Red() = default;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Inicio del proyecto de simulación de red\n";
+    return 0;
+}
